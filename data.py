e10cb7aabfc94061f5b3b1e908d837100f260f56
Oğuzhan Töre Özyürek <torefordev@gmail.com>
2023-06-15 17:16:31 +0300

    questions changed

--- data.py
+++ data.py
@@ -1,26 +1,62 @@
 question_data = [
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The logo for Snapchat is a Bell.", "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "RAM stands for Random Access Memory.", "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "&quot;HTML&quot; stands for Hypertext Markup Language.", "correct_answer": "True",
-     "incorrect_answers": ["False"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-                                       "question": "Time on Computers is measured via the EPOX System.",
-                                       "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The Windows 7 operating system has six main editions.", "correct_answer": "True",
-     "incorrect_answers": ["False"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-                                       "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
-                                       "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
-     "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
-     "correct_answer": "True", "incorrect_answers": ["False"]}]
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "English new wave musician Gary Numan founded the video game development company Facepunch Studios in March 2009.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Luigi is taller than Mario?",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Niko Bellic is the protagonist of Grand Theft Auto IV.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Deus Ex (2000) does not feature the World Trade Center because it was destroyed by terrorist attacks according to the game's plot.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The 2005 video game \"Call of Duty 2: Big Red One\" is not available on PC.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "Entertainment: Video Games",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "In the game Dead by Daylight, the killer Michael Myers is refered to as \"The Shape\".",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        }
+    ]
